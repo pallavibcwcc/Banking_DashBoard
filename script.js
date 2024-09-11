b4d7b8334818a4c6600972c6c88ae7201d8b55a7
Gaurav <gaurav@Gauravs-MacBook-Pro.local>
2024-09-11 14:42:12 +0530

    changes in button

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 //script.js file
 const currVal = 1000;
-let amount = document.getElementById("amount").innerHTML=currVal;
+let amount = (document.getElementById("amount").innerHTML = currVal);
 
 amount.innerText = currVal;
 
-function deposit(){
-    let DepositInp=document.getElementById("DepositIn").value;
-    console.log(DepositInp,amount,"DepositIn");
-    let DepositIn= Number(DepositInp)+amount;
-   
-    console.log(DepositIn,"amount");
+//function for add value when deopsit button clicked by user
+function deposit() {
+  let DepositInp = document.getElementById("DepositIn").value;
+  console.log(DepositInp, amount, "DepositIn");
+  let DepositIn = Number(DepositInp) + amount;
 
-    if(DepositInp<=0||amount==0){
-       alert('No a Valid Number');
+  console.log(DepositIn, "amount");
 
-    }else{
-        amount = document.getElementById("amount").innerHTML=DepositIn;
-        let DepositInp=document.getElementById("DepositIn").value="";
-    } 
+  if (DepositInp <= 0 || amount == 0) {
+    alert("No a Valid Number");
+  } else {
+    amount = document.getElementById("amount").innerHTML = DepositIn;
+    let DepositInp = (document.getElementById("DepositIn").value = "");
+  }
 }
-function Withdrawalamount(){
-    let WithdrawalInp=document.getElementById("WithdrawalIn").value;
-    console.log(WithdrawalInp,amount,"WithdrawalIn");
-    let WithdrawalIn=Number(WithdrawalInp)-amount;
-   
-    console.log(WithdrawalIn,"amount");
+//function for deduct value from current balance when Withdrawal button is clicked
+function Withdrawalamount() {
+  let WithdrawalInp = document.getElementById("WithdrawalIn").value;
+  console.log(WithdrawalInp, amount, "WithdrawalIn");
+  let WithdrawalIn = Number(WithdrawalInp) - amount;
 
-    if(WithdrawalInp<=0)
-    {
-        alert('Not a valid Number');
-    }if (amount==0) {
-        alert('No change should occur to the balance');   
-    } 
-    else{
-        amount=document.getElementById("amount").innerHTML=WithdrawalIn;   
-        
-    let WithdrawalInp=document.getElementById("WithdrawalIn").value="";  
-    }
+  console.log(WithdrawalIn, "amount");
 
+  if (WithdrawalInp <= 0) {
+    alert("Not a valid Number");
+  }
+  if (amount == 0) {
+    alert("No change should occur to the balance");
+  } else {
+    amount = document.getElementById("amount").innerHTML = WithdrawalIn;
+
+    let WithdrawalInp = (document.getElementById("WithdrawalIn").value = "");
+  }
 }
-
